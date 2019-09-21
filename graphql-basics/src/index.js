8cba66b87acf463e013b4865d5d25104932d0e11
David Park <dpark4657@gmail.com>
2019-09-21 16:50:45 -0700

    implements basic graphql-yoga server

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,38 @@
-console.log('Hello GraphQL')
+import { GraphQLServer } from 'graphql-yoga';
+
+// Type definition (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+    name: String!
+    location: String!
+    bio: String!
+  }
+`;
+
+// Resolvers (functions to get data)
+const resolvers = {
+  Query: {
+    hello() {
+      return 'This is my first query';
+    },
+    name() {
+      return 'David Park';
+    },
+    location() {
+      return 'San Francisco';
+    },
+    bio() {
+      return 'I am a person';
+    },
+  },
+};
+
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers,
+});
+
+server.start(() => {
+  console.log('The server is up!');
+});
